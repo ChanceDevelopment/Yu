ca304a8007c70678a4f33edc148100601a7adc74
kobe1081 <hdming1081@gmail.com>
2016-10-19 15:23:27 +0800

    1

--- ThirdPartySDK/SMS_SDK/MOBFoundation.framework/Headers/MOBFDebug.h
+++ ThirdPartySDK/SMS_SDK/MOBFoundation.framework/Headers/MOBFDebug.h
@@ -8,10 +8,31 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, MOBFDebugLevel)
+{
+    MOBFDebugLevelApp   = 0x0001,
+};
+
 /**
  *  调试工具类
  */
 @interface MOBFDebug : NSObject
+
+/**
+ *  启用调试模式
+ *
+ *  @param enabled YES 启用调试，NO 禁用调试模式
+ */
++ (void)enabledDebug:(BOOL)enabled;
+
+/**
+ *  启动调试模式
+ *
+ *  @param enabled YES 启动调试，NO 禁用调试模式
+ *  @param level   调试级别
+ */
++ (void)enabledDebug:(BOOL)enabled
+               level:(MOBFDebugLevel)level;
 
 /**
  *  输出调试日志
@@ -19,13 +40,13 @@
  *  @param format 日志格式
  *  @param ... 日志参数
  */
-+ (void)log:(NSString *)format, ... NS_FORMAT_FUNCTION(1, 2);
++ (void)log:(NSString *)format,...NS_FORMAT_FUNCTION(1,2);
 
 /**
- *  观察释放对象，指定类型的被释放对象会打印在Console中。
+ *  输出调试日志
  *
- *  @param type 对象类型
+ *  @param format 日志格式
  */
-+ (void)watchDeallocObjectWithType:(Class)type;
++ (void)sdkLog:(NSString *)format,...NS_FORMAT_FUNCTION(1,2);
 
 @end
